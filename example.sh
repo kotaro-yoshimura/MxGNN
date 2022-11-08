62da85fb312a2e9817b7606161b7a52955864f32
kotaro-yoshimura <98009545+kotaro-yoshimura@users.noreply.github.com>
2022-11-08 10:38:26 +0900

    Update example.sh

--- example.sh
+++ example.sh
@@ -1,5 +1,5 @@
 #NCI109 -MxGNN
-python -m train --bmname=NCI109 --method=MxGNN --assign-ratio 0.1 0.1 0.1 --hidden-dim 30 50 80 --output-dim 30 50 80 --cuda=0 --num-classes=2 --num-aspect=3 --multi-conv=1 --multi-pool=0 --max-nodes=100  --epochs=1000 --dropout=0.0 --batch-size=20 --lr=0.001
+python -m train --bmname=NCI109 --method=MxGNN --assign-ratio 0.1 0.1 0.1 --hidden-dim 30 50 80 --output-dim 30 50 80 --cuda=1 --num-classes=2 --num-aspect=3 --multi-conv=1 --multi-pool=0 --max-nodes=100  --epochs=1000 --dropout=0.0 --batch-size=20 --lr=0.001
 
 #PROTEINS -MxGNN
 # python -m train --bmname=PROTEINS --method=MxGNN --assign-ratio 0.1 0.1 0.1 --hidden-dim 20 40 60 --output-dim 20 40 60 --cuda=0 --num-classes=2 --num-aspect=3 --multi-conv=1 --multi-pool=0 --max-nodes=200  --epochs=300 --dropout=0.0 --batch-size=20
