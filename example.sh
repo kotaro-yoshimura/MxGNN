57ad9ab52e69301e97d3c816f153bb9598f896b5
kotaro-yoshimura <98009545+kotaro-yoshimura@users.noreply.github.com>
2022-11-09 14:51:35 +0900

    Update example.sh

--- example.sh
+++ example.sh
@@ -11,7 +11,7 @@
 # python -m train --bmname=DD --method=GNN --assign-ratio 0.1 0.1 0.1 --hidden-dim 20 40 60 --output-dim 20 40 60 --cuda=0 --num-classes=2 --num-aspect=3 --multi-conv=1 --multi-pool=0 --max-nodes=500  --epochs=500 --dropout=0.0 --batch-size=20
 
 #ENZYMES -MxGNN
-# python -m train --bmname=ENZYMES --method=MxGNN --assign-ratio 0.15 0.15 --hidden-dim 30 80 --output-dim 30 80 --cuda=0 --num-classes=6 --num-aspect=2 --multi-conv=1 --multi-pool=0 --max-nodes=100  --epochs=300 --dropout=0.0 --batch-size=20
+python -m train --bmname=NCI109 --method=MxGNN --assign-ratio 0.15 0.15 --hidden-dim 30 80 --output-dim 30 80 --cuda=0 --num-classes=6 --num-aspect=2 --multi-conv=1 --multi-pool=0 --max-nodes=100  --epochs=300 --dropout=0.0 --batch-size=20
 
 # python -m train --bmname=ENZYMES --method=MxGNN --assign-ratio 0.15 0.15 0.15 --hidden-dim 30 50 80 --output-dim 30 50 80 --cuda=0 --num-classes=6 --num-aspect=3 --multi-conv=1 --multi-pool=0 --max-nodes=100  --epochs=300 --dropout=0.0 --batch-size=20
 
@@ -19,4 +19,4 @@
 
 # python -m train --bmname=ENZYMES --method=MxGNN --assign-ratio 0.15 0.15 0.15 0.15 0.15 --hidden-dim 10 30 50 80 100 --output-dim 10 30 50 80 100 --cuda=0 --num-classes=6 --num-aspect=5 --multi-conv=1 --multi-pool=0 --max-nodes=100  --epochs=300 --dropout=0.0 --batch-size=20
 
-python -m train --bmname=ENZYMES --method=MxGNN --assign-ratio 0.15 0.15 0.15 0.15 0.15 0.15 --hidden-dim 10 30 50 80 100 120 --output-dim 10 30 50 80 100 120 --cuda=0 --num-classes=6 --num-aspect=6 --multi-conv=1 --multi-pool=0 --max-nodes=100  --epochs=300 --dropout=0.0 --batch-size=20
+# python -m train --bmname=ENZYMES --method=MxGNN --assign-ratio 0.15 0.15 0.15 0.15 0.15 0.15 --hidden-dim 10 30 50 80 100 120 --output-dim 10 30 50 80 100 120 --cuda=0 --num-classes=6 --num-aspect=6 --multi-conv=1 --multi-pool=0 --max-nodes=100  --epochs=300 --dropout=0.0 --batch-size=20
