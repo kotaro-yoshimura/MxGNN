cf680f78a23075df8b274f710729dc131a6b1d9a
kotaro-yoshimura <98009545+kotaro-yoshimura@users.noreply.github.com>
2022-11-10 13:24:19 +0900

    Update example.sh

--- example.sh
+++ example.sh
@@ -13,7 +13,7 @@
 #ENZYMES -MxGNN
 # python -m train --bmname=NCI109 --method=MxGNN --assign-ratio 0.15 0.15 --hidden-dim 30 80 --output-dim 30 80 --cuda=0 --num-classes=6 --num-aspect=2 --multi-conv=1 --multi-pool=0 --max-nodes=100  --epochs=300 --dropout=0.0 --batch-size=20
 
-python -m train --bmname=NCI109 --method=MxGNN --assign-ratio 0.15 0.15 0.15 --hidden-dim 30 50 80 --output-dim 30 50 80 --cuda=0 --num-classes=6 --num-aspect=3 --multi-conv=1 --multi-pool=0 --max-nodes=100  --epochs=300 --dropout=0.0 --batch-size=20
+python -m train --bmname=NCI109 --method=MxGNN --assign-ratio 0.15 0.15 0.15 --hidden-dim 30 50 80 --output-dim 30 50 80 --cuda=1 --num-classes=6 --num-aspect=3 --multi-conv=1 --multi-pool=0 --max-nodes=100  --epochs=300 --dropout=0.0 --batch-size=20
 
 # python -m train --bmname=ENZYMES --method=MxGNN --assign-ratio 0.15 0.15 0.15 0.15 --hidden-dim 30 50 80 100 --output-dim 20 40 70 100 --cuda=0 --num-classes=6 --num-aspect=4 --multi-conv=1 --multi-pool=0 --max-nodes=100  --epochs=300 --dropout=0.0 --batch-size=20
 
